199b0a6bd65f9030c1941440774d2243e86c13ca
James Ransome <jamesransome@msn.com>
2017-11-28 13:55:46 +0000

    Refactor OutputPrinter to not hardcode headers

--- src/OutputPrinter.js
+++ src/OutputPrinter.js
@@ -1,9 +1,14 @@
-function OutputPrinter() {}
+function OutputPrinter(wordHeader, occurrencesHeader, isPrimeHeader, divider) {
+  this.wordHeader = wordHeader || "Word: ";
+  this.occurrencesHeader = occurrencesHeader || "Occurrences: ";
+  this.isPrimeHeader = isPrimeHeader || "Is the number of occurrences a prime number? ";
+  this.divider = divider || " | ";
+}
 
 OutputPrinter.prototype.printWordCountResult = function(word, occurrences, isPrime){
-  let outputString = "Word: " + word + " | ";
-  outputString += ("Occurrences: " + occurrences + " | ");
-  outputString += ("Is the number of occurrences a prime number? " + isPrime);
+  let outputString = this.wordHeader + word + this.divider;
+  outputString += (this.occurrencesHeader + occurrences + this.divider);
+  outputString += (this.isPrimeHeader + isPrime);
   return outputString;
 };
 

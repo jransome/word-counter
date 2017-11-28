946e261c4549a545d9a9d87720fc8614088a1e6d
James Ransome <jamesransome@msn.com>
2017-11-28 19:05:22 +0000

    Update TextFileReaderSpec with different file content

--- spec/TextFileReaderSpec.js
+++ spec/TextFileReaderSpec.js
@@ -2,13 +2,13 @@ describe("TextFileReader", function(){
   var TextFileReader = require('../src/TextFileReader');
   var textFileReader;
   var mockFilePath = 'spec/mock-data/mockTextFile.txt';
-  var mockFileContent = "This is a mock text file. It's not very long.\n";
+  var expectedFileContent = "This is a very very short story.\n";
 
   beforeEach(function(){
     textFileReader = new TextFileReader();
   });
 
   it("reads a text file and returns a string containing its contents", function(){
-    expect(textFileReader.readFile(mockFilePath)).toEqual(mockFileContent);
+    expect(textFileReader.readFile(mockFilePath)).toEqual(expectedFileContent);
   });
 });
